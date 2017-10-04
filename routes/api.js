5766ff98ef370df5d096006848f7ab5bb2590694
Eric Abell <ericabell@users.noreply.github.com>
2017-10-04 10:33:59 -0500

    Adding basic post to /login to check credentials

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
 
+const User = require('../models/users');
+
 /* basic status. */
 router.get('/status', function(req, res, next) {
   res.json({status: 'ok'});
 });
+
+/* authenticate a user/pass pair */
+router.post('/login', function( req, res, status ) {
+  // expect username/pass in body as json
+  if(req.body.username && req.body.password) {
+    // validate against users collection
+    User.find({username: req.body.username})
+      .then( (result) => {
+        if( result.authenticate(req.body.password) ) {
+          res.json({status: 'success'})
+        } else {
+          res.json({status: 'failure'})
+        }
+      })
+  }
+})
 
 module.exports = router;
